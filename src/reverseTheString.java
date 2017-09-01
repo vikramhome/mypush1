297e02706ad85ad9b4683ec7dee1b6be4ebb7613
vikram <vikramhome2@gmail.com>
2017-09-01 07:11:55 -0500

    remove ill chars

--- src/reverseTheString.java
+++ src/reverseTheString.java
@@ -1,3 +1,17 @@
-public class reverseTheString {  public static void main(String[] args) {   String str = "MyJava";  System.out.println(recursiveMethod(str));      //Output : avaJyM }        
-   static String recursiveMethod(String str)    { if ((null == str) || (str.length() <= 1))   {   return str;  } return recursiveMethod(str.substring(1)) + str.charAt(0);  }   
-  } 
+public class reverseTheString
+{
+        public static void main(String[] args)
+        {   
+		String str = "MyJava";
+		System.out.println(recursiveMethod(str));
+		//Output : avaJyM
+	}
+	static String recursiveMethod(String str)
+	{
+		if ((null == str) || (str.length() <= 1))
+		{
+			return str;
+		}
+		return recursiveMethod(str.substring(1)) + str.charAt(0);
+	}
+}
